07ac869c890b2ae19ce95c28ad7fe0746f333257
YaseenShawkat <Yaseen@gmail.com>
2021-02-05 11:26:28 +0200

    Created a function file

---
+++ function.cpp
@@ -0,0 +1,20 @@
+void printBoard(int board[N][N]) {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+         cout << board[i][j] << " ";
+         cout << endl;
+   }
+}
+bool isValid(int board[N][N], int row, int col) {
+   for (int i = 0; i < col; i++) //check whether there is queen in the left or not
+      if (board[row][i])
+         return false;
+   for (int i=row, j=col; i>=0 && j>=0; i--, j--)
+      if (board[i][j]) //check whether there is queen in the left upper diagonal or not
+         return false;
+   for (int i=row, j=col; j>=0 && i<N; i++, j--)
+      if (board[i][j]) //check whether there is queen in the left lower diagonal or not
+         return false;
+   return true;
+}
+
